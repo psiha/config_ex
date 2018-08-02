a65ffb7070ceeb6412de1f9b32b0d2eb95520127
Nenad Mikša <nenad.miksa@microblink.com>
2018-08-02 11:12:48 +0200

    Revert "version bump"
    This reverts commit 3a81e737e266f8fb804fd271137f1f3c472e15d4.

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, CMake, tools
 
 class ConfigExConan(ConanFile):
     name = "ConfigEx"
-    version = "1.0.1"
+    version = "1.0.0"
     requires = 'boost/[>=1.67]@conan/stable'
     license = "MIT"
     url = "https://github.com/microblink/config_ex"
